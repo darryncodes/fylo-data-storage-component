2e72dc2ebad0caa10b7eafd052587eff5d07afcf
Darryn Codes <hello@darryncodes.co.uk>
2022-01-14 09:05:28 +0000

    functionality - adding js logic for count up animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-// document.getElementById("storage").disabled = true;
+const amount = document.querySelector(".popup__amount");
+
+function updateCount() {
+    const count = parseInt(amount.innerText);
+    const target = parseInt(amount.getAttribute("data-target"));
+    const increment = 1;
+
+    if (count < target) {
+        amount.innerText = count + increment;
+        setTimeout(updateCount, 5);
+    } else {
+        amount.innerText = target;
+    }
+    console.log(count);
+}
+
+setTimeout(updateCount, 2600);
